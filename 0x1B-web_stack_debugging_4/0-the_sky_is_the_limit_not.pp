50a6f5f02c0789009d054fced6f673575cb7f2d5
godfrey-creat <ogodfrey135@gmail.com>
2023-08-16 00:05:22 -0700

    debugging web stack to accomodate user holberton

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# The manuscript increases the amount of traffic an Nginx server can handle
+
+# Increase the ULIMIT of the default file
+file { 'fix-for-nginx':
+  ensure  => 'file',
+  path    => '/etc/default/nginx',
+  content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
+}
+
+# Restarting Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
+}
